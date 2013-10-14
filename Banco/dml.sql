ed7c6d453d012e8ceb2f9b2ea4b0d39ab39e1ed9
Bezerra <rafaelgustavo@hotmail.com>
2013-10-14 17:21:13 -0300

    script do banco

---
+++ Banco/dml.sql
@@ -0,0 +1,8 @@
+use RankingFilmes;
+insert into filme (fil_nome) values ('Avatar');
+insert into filme (fil_nome) values ('Titanic');
+insert into filme (fil_nome) values ('Os Vingadores');
+insert into filme (fil_nome) values ('Harry Potter e as relíquias da morte');
+insert into filme (fil_nome) values ('Homem de ferro 3');
+
+
