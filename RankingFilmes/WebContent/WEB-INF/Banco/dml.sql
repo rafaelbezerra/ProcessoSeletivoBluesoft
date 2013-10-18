20774a0240f2b47998aa01c2d47e3405b29854d1
Bezerra <rafaelgustavob@hotmail.com>
2013-10-18 18:59:51 -0300

    removido libs e implementado maven, testado localmente

--- RankingFilmes/WebContent/WEB-INF/Banco/dml.sql
+++
@@ -1,8 +0,0 @@
-use RankingFilmes;
-insert into filme (fil_nome) values ('Avatar');
-insert into filme (fil_nome) values ('Titanic');
-insert into filme (fil_nome) values ('Os Vingadores');
-insert into filme (fil_nome) values ('Harry Potter e as relíquias da morte');
-insert into filme (fil_nome) values ('Homem de ferro 3');
-
-
