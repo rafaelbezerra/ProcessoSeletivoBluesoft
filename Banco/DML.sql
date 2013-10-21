1aa9ad081ab3c906806dc60cb284e945f5ff9b7b
rafaelbezerra <rafaelgustavob@hotmail.com>
2013-10-21 13:54:59 -0200

    Create DML.sql

---
+++ Banco/DML.sql
@@ -0,0 +1,6 @@
+use rankingfilmes;
+insert into filme (fil_nome) values ('Avatar');
+insert into filme (fil_nome) values ('Titanic');
+insert into filme (fil_nome) values ('Os Vingadores');
+insert into filme (fil_nome) values ('Harry Potter e as relíquias da morte');
+insert into filme (fil_nome) values ('Homem de ferro 3');
